80b777a480879b170050597109437a9694368d17
AndreNarvell <andrenarvell@hotmail.com>
2022-03-31 15:46:05 +0200

    added contaxt hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,61 +3,27 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import ListScreen from "./components/ListScreen";
 import FocusScreen from "./components/FocusScreen";
 import { Task } from "./models/types";
-import { shuffle } from "lodash";
-import { nanoid } from "nanoid";
+
 import useLocalStorage from "./hooks/use-local-storage";
-import { useState } from "react";
+
+import TaskContext from "./contexts/task-store";
 
 function App() {
   const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", []);
-  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(
-    undefined
-  );
-
-  const addTask = (task: Pick<Task, "label">) => {
-    const id = nanoid();
-    setTasks((tasks) => [
-      ...tasks,
-      { id, label: task.label, isComplete: false },
-    ]);
-    if (!focusedTaskId) setFocusedTaskId(id);
-  };
-
-  const updateTaskCompletion = (taskId: string, isComplete: boolean) => {
-    setTasks((tasks) =>
-      tasks.map((task) => {
-        if (task.id === taskId) return { ...task, isComplete };
-        return task;
-      })
-    );
-  };
-
-  const focusedTask = tasks.find((task) => task.id === focusedTaskId);
-
-  const shuffleFocusedTask = () => {
-    setFocusedTaskId(shuffle(tasks.filter((task) => !task.isComplete))[0]?.id);
-  };
-
-  const tasksApi = {
-    addTask,
-    focusedTask,
-    tasks,
-    setTasks,
-    shuffleFocusedTask,
-    updateTaskCompletion,
-  };
 
   return (
     <div>
       <BrowserRouter>
-        <nav>
-          <NavLink to="/">List</NavLink>
-          <NavLink to="/focus">Focus</NavLink>
-        </nav>
-        <Routes>
-          <Route path="/" element={<ListScreen {...tasksApi} />} />
-          <Route path="/focus" element={<FocusScreen {...tasksApi} />} />
-        </Routes>
+        <TaskContext.Provider value={[tasks, setTasks]}>
+          <nav>
+            <NavLink to="/">List</NavLink> {""} - {""}
+            <NavLink to="/focus">Focus</NavLink>
+          </nav>
+          <Routes>
+            <Route path="/" element={<ListScreen />} />
+            <Route path="/focus" element={<FocusScreen />} />
+          </Routes>
+        </TaskContext.Provider>
       </BrowserRouter>
     </div>
   );
